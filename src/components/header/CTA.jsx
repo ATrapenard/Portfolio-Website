1a84ed8e26719902bb6406aec56211835e5d488f
Alan Hussein Trapenard <alan.trapenard@gmail.com>
2023-12-18 16:20:31 -0800

    Changed method of resume display

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import CV from "../../assets/FinalResume.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn btn-download">
-        Download CV
+      <a
+        href="https://docs.google.com/document/d/1wkv7dNfodfobP9AjuQ2FosgmAkU0SewV/edit?usp=sharing&ouid=101114177116630609129&rtpof=true&sd=true"
+        target="_blank"
+        download
+        className="btn btn-download">
+        My Resume
       </a>
       <a
         href="#contact"
